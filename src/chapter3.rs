801bb20022788de839fee583ca19c6f43825c83f
xcesoo <notxceso@gmail.com>
2024-10-18 18:59:01 +0300

    chapter_3_all

--- src/chapter3.rs
+++ src/chapter3.rs
@@ -1,21 +1,109 @@
 /// https://practice.course.rs/variables.html
-#[test]
-fn test31() {
-    let x: i32 = 5; // Uninitialized but used, ERROR !
-    let _y: i32; // Uninitialized but also unused, only a Warning !
 
+#[test]
+// Fix the error below with least amount of modification to the code
+fn test3_1()
+{
+    let x = 5;
     assert_eq!(x, 5);
     println!("Success!");
-
+}
+#[test]
+// Fill the blanks in the code to make it compile
+fn test3_2()
+{
+    let mut x =1;
+    x+=2;
+    assert_eq!(x, 3);
+    println!("Success!");
+}
+#[test]
+// Fix the error below with least amount of modification
+fn test3_3()
+{
+    let x: i32 = 10;
+    let y: i32;
+    {
+        y= 5;
+        println!("The value of x is {} and value of y is {}", x, y);
+    }
+    println!("The value of x is {} and value of y is {}", x, y);
 }
 
 #[test]
-fn test399() {
-    let x = 5;
-    let y = 10;
+// Fix the error with the use of define_x
+fn test3_4()
+{
+    println!("{}, world", define_x());
+}
+fn define_x() -> &'static str
+{
+    let x = "hello";
+    return x;
+}
 
-    let real = x + y;
-    let expected = 15;
+#[test]
+// Only modify `assert_eq!` to make the `println!` work(print `42` in terminal)
+fn test3_5()
+{
+    let x: i32 = 5;
+    {
+        let x = 12;
+        assert_eq!(x, 12);
+    }
 
-    assert_eq!(real, expected);
+    assert_eq!(x, 5);
+
+    let x = 42;
+    println!("{}", x); // Prints "42".
+}
+
+#[test]
+fn test3_6()
+{
+    let x: i32 = 7;
+    let x = x + 3;
+
+    let _y = 4;
+    let _y = "I can also be bound to text!";
+
+    println!("Success! Value of x is: {}", x);
+    println!("Success! Value of y is: {}", _y);
+}
+
+#[test]
+fn test3_7_1()
+{
+    let x = 1;
+    for i in 1..10
+    {
+        println!("{}", x*i);
+    }
+}
+#[test]
+fn test3_7_2()
+{
+    let x:i32 = 2;
+    println!("Value of x is: {}", x.pow(10))
+}
+#[test]
+fn test3_8()
+{
+        let (mut x, y) = (1, 2);
+        x += 2;
+        assert_eq!(x, 3);
+        assert_eq!(y, 2);
+
+        println!("Success!");
+}
+#[test]
+fn test3_9()
+{
+    let (x, y);
+    (x,..) = (3, 4);
+    [.., y] = [1, 2];
+    // Fill the blank to make the code work
+    assert_eq!([x,y], [3,2]);
+
+    println!("Success!");
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod chapter3;
 mod chapter4;
 mod chapter5;
 mod chapter6;
+mod chapter041;
 
 fn main() {
     println!("Hello, world!");
